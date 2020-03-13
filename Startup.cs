b1aa4dbfeb5cf6c7e61c366be10acc75c395f4ad
unknown <ilyavusnya@gmail.com>
2020-03-13 11:46:18 +0200

    Commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ComputerClub.Startup))]
+namespace ComputerClub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
